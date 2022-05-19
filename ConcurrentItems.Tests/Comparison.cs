00718c4d832bd085edceeb15977d8f3b95cf9bf0
artemiusgreat <arteinvolo@gmail.com>
2022-05-19 02:47:37 -0400

    Setup

---
+++ ConcurrentItems.Tests/Comparison.cs
@@ -0,0 +1,233 @@
+using System;
+using System.Collections.Generic;
+
+namespace ConcurrentItems.Tests
+{
+  [TestClass]
+  public class Comparison
+  {
+    [TestMethod]
+    public void Count()
+    {
+      var x = new List<int> { 1, 10, 5, 25 };
+      var y = new ConcurrentItems<int> { 1, 10, 5, 25 };
+
+      Assert.AreEqual(x.Count, y.Count);
+    }
+
+    [TestMethod]
+    public void Creation()
+    {
+      var x = new List<int> { 1, 10, 5, 25 };
+      var y = new ConcurrentItems<int> { 1, 10, 5, 25 };
+
+      Assert.AreEqual(string.Join(",", x), string.Join(",", y));
+    }
+
+    [TestMethod]
+    public void Addition()
+    {
+      var x = new List<int> { 1, 10, 5, 25 };
+      var y = new ConcurrentItems<int> { 1, 10, 5, 25 };
+
+      x.Add(100);
+      y.Add(100);
+
+      Assert.AreEqual(string.Join(",", x), string.Join(",", y));
+    }
+
+    [TestMethod]
+    public void Clear()
+    {
+      var x = new List<int> { 1, 10, 5, 25 };
+      var y = new ConcurrentItems<int> { 1, 10, 5, 25 };
+
+      x.Clear();
+      y.Clear();
+
+      Assert.AreEqual(string.Join(",", x), string.Join(",", y));
+    }
+
+    [TestMethod]
+    public void Deletion()
+    {
+      var x = new List<int> { 1, 10, 5, 25 };
+      var y = new ConcurrentItems<int> { 1, 10, 5, 25 };
+
+      // Remove non-existent
+
+      x.Remove(100);
+      y.Remove(100);
+
+      Assert.AreEqual(string.Join(",", x), string.Join(",", y));
+
+      // Remove in the middle
+
+      x.Remove(5);
+      y.Remove(5);
+
+      Assert.AreEqual(string.Join(",", x), string.Join(",", y));
+
+      // Remove first
+
+      x.Remove(1);
+      y.Remove(1);
+
+      Assert.AreEqual(string.Join(",", x), string.Join(",", y));
+
+      // Remove last
+
+      x.Remove(25);
+      y.Remove(25);
+
+      Assert.AreEqual(string.Join(",", x), string.Join(",", y));
+    }
+
+    [TestMethod]
+    public void IndexDeletion()
+    {
+      var x = new List<int> { 1, 10, 5, 25 };
+      var y = new ConcurrentItems<int> { 1, 10, 5, 25 };
+
+      var exceptionX = 0.GetType();
+      var exceptionY = 0.ToString().GetType();
+
+      // Out of range index 
+
+      try { x.RemoveAt(-1); } catch (Exception e) { exceptionX = e.GetType(); }
+      try { y.RemoveAt(-1); } catch (Exception e) { exceptionY = e.GetType(); }
+
+      Assert.AreEqual(exceptionX, exceptionY);
+      Assert.AreEqual(string.Join(",", x), string.Join(",", y));
+
+      exceptionX = 0.GetType();
+      exceptionY = 0.ToString().GetType();
+
+      try { x.RemoveAt(x.Count); } catch (Exception e) { exceptionX = e.GetType(); }
+      try { y.RemoveAt(y.Count); } catch (Exception e) { exceptionY = e.GetType(); }
+
+      Assert.AreEqual(exceptionX, exceptionY);
+      Assert.AreEqual(string.Join(",", x), string.Join(",", y));
+
+      // Remove in the middle
+
+      x.RemoveAt(2);
+      y.RemoveAt(2);
+
+      Assert.AreEqual(string.Join(",", x), string.Join(",", y));
+
+      // Remove first
+
+      x.RemoveAt(0);
+      y.RemoveAt(0);
+
+      Assert.AreEqual(string.Join(",", x), string.Join(",", y));
+
+      // Remove last
+
+      x.RemoveAt(x.Count - 1);
+      y.RemoveAt(y.Count - 1);
+
+      Assert.AreEqual(string.Join(",", x), string.Join(",", y));
+    }
+
+    [TestMethod]
+    public void Indices()
+    {
+      var x = new List<int> { 1, 10, 5, 25 };
+      var y = new ConcurrentItems<int> { 1, 10, 5, 25 };
+
+      var exceptionX = 0.GetType();
+      var exceptionY = 0.ToString().GetType();
+
+      // Out of range index 
+
+      try { x[-1] = 0; } catch (Exception e) { exceptionX = e.GetType(); }
+      try { y[-1] = 0; } catch (Exception e) { exceptionY = e.GetType(); }
+
+      Assert.AreEqual(exceptionX, exceptionY);
+      Assert.AreEqual(string.Join(",", x), string.Join(",", y));
+
+      exceptionX = 0.GetType();
+      exceptionY = 0.ToString().GetType();
+
+      try { x[x.Count] = 0; } catch (Exception e) { exceptionX = e.GetType(); }
+      try { y[y.Count] = 0; } catch (Exception e) { exceptionY = e.GetType(); }
+
+      Assert.AreEqual(exceptionX, exceptionY);
+      Assert.AreEqual(string.Join(",", x), string.Join(",", y));
+
+      // Update some of existing indexes
+
+      x[0] = 100;
+      x[2] = 20;
+
+      y[0] = 100;
+      y[2] = 20;
+
+      Assert.AreEqual(string.Join(",", x), string.Join(",", y));
+
+      // Update all indexes 
+
+      x[0] = 100;
+      x[1] = 50;
+      x[2] = 20;
+      x[3] = 0;
+
+      y[0] = 100;
+      y[1] = 50;
+      y[2] = 20;
+      y[3] = 0;
+
+      Assert.AreEqual(string.Join(",", x), string.Join(",", y));
+    }
+
+    [TestMethod]
+    public void Insertion()
+    {
+      var x = new List<int> { 1, 10, 5, 25 };
+      var y = new ConcurrentItems<int> { 1, 10, 5, 25 };
+
+      var exceptionX = 0.GetType();
+      var exceptionY = 0.ToString().GetType();
+
+      // Out of range index 
+
+      try { x.Insert(-1, 0); } catch (Exception e) { exceptionX = e.GetType(); }
+      try { y.Insert(-1, 0); } catch (Exception e) { exceptionY = e.GetType(); }
+
+      Assert.AreEqual(exceptionX, exceptionY);
+      Assert.AreEqual(string.Join(",", x), string.Join(",", y));
+
+      exceptionX = 0.GetType();
+      exceptionY = 0.ToString().GetType();
+
+      try { x.Insert(x.Count + 1, 0); } catch (Exception e) { exceptionX = e.GetType(); }
+      try { y.Insert(y.Count + 1, 0); } catch (Exception e) { exceptionY = e.GetType(); }
+
+      Assert.AreEqual(exceptionX, exceptionY);
+      Assert.AreEqual(string.Join(",", x), string.Join(",", y));
+
+      // Insert in the middle
+
+      x.Insert(2, 30);
+      y.Insert(2, 30);
+
+      Assert.AreEqual(string.Join(",", x), string.Join(",", y));
+
+      // Insert first
+
+      x.Insert(0, 50);
+      y.Insert(0, 50);
+
+      Assert.AreEqual(string.Join(",", x), string.Join(",", y));
+
+      // Insert last
+
+      x.Insert(x.Count, 90);
+      y.Insert(y.Count, 90);
+
+      Assert.AreEqual(string.Join(",", x), string.Join(",", y));
+    }
+  }
+}
---
+++ ConcurrentItems/ConcurrentItems.cs
@@ -0,0 +1,223 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+
+public class ConcurrentItems<T> : IList<T>
+{
+  protected ConcurrentQueue<T> _items = new();
+  protected ConcurrentDictionary<int, T> _indices = new();
+
+  /// <summary>
+  /// Index
+  /// </summary>
+  /// <param name="index"></param>
+  /// <returns></returns>
+  public T this[int index]
+  {
+    get => _indices.TryGetValue(index, out T value) ? value : default;
+    set => UpdateInRange(index, 0, _items.Count, () => _indices[index] = value);
+  }
+
+  /// <summary>
+  /// Count
+  /// </summary>
+  public int Count => _items.Count;
+
+  /// <summary>
+  /// Mutable
+  /// </summary>
+  public bool IsReadOnly => false;
+
+  /// <summary>
+  /// Add
+  /// </summary>
+  /// <param name="input"></param>
+  public void Add(T input) => _items.Enqueue(input);
+
+  /// <summary>
+  /// Contains
+  /// </summary>
+  /// <param name="input"></param>
+  /// <returns></returns>
+  public bool Contains(T input) => _items.Contains(input);
+
+  /// <summary>
+  /// Copy
+  /// </summary>
+  /// <param name="items"></param>
+  /// <param name="index"></param>
+  public void CopyTo(T[] items, int index) => _items.CopyTo(items, index);
+
+  /// <summary>
+  /// Enumerator
+  /// </summary>
+  /// <returns></returns>
+  public IEnumerator<T> GetEnumerator() => _items.GetEnumerator();
+
+  /// <summary>
+  /// Search
+  /// </summary>
+  /// <param name="input"></param>
+  /// <returns></returns>
+  public int IndexOf(T input) => _items.ToList().IndexOf(input);
+
+  /// <summary>
+  /// Clear
+  /// </summary>
+  public void Clear()
+  {
+    _items.Clear();
+    _indices.Clear();
+  }
+
+  /// <summary>
+  /// Insert
+  /// </summary>
+  /// <param name="index"></param>
+  /// <param name="input"></param>
+  public void Insert(int index, T input)
+  {
+    if (index == _items.Count)
+    {
+      _items.Enqueue(input);
+      _indices[_items.Count - 1] = input;
+
+      return;
+    }
+
+    UpdateInRange(index, 0, _items.Count + 1, () =>
+    {
+      _indices.Clear();
+
+      var i = 0;
+      var items = new ConcurrentQueue<T>();
+
+      while (_items.TryDequeue(out T item))
+      {
+        if (Equals(i, index))
+        {
+          items.Enqueue(input);
+          _indices[items.Count - 1] = input;
+        }
+
+        items.Enqueue(item);
+        _indices[items.Count - 1] = item;
+
+        i++;
+      }
+
+      _items = items;
+    });
+  }
+
+  /// <summary>
+  /// Remove
+  /// </summary>
+  /// <param name="input"></param>
+  /// <returns></returns>
+  public bool Remove(T input)
+  {
+    _indices.Clear();
+
+    var response = false;
+    var items = new ConcurrentQueue<T>();
+
+    while (_items.TryDequeue(out T item))
+    {
+      if (Equals(input, item) is false)
+      {
+        response = true;
+        items.Enqueue(item);
+        _indices[items.Count - 1] = item;
+      }
+    }
+
+    _items = items;
+
+    return response;
+  }
+
+  /// <summary>
+  /// Remove by index
+  /// </summary>
+  /// <param name="index"></param>
+  public void RemoveAt(int index)
+  {
+    UpdateInRange(index, 0, _items.Count, () =>
+    {
+      _indices.Clear();
+
+      var i = 0;
+      var items = new ConcurrentQueue<T>();
+
+      while (_items.TryDequeue(out T item))
+      {
+        if (Equals(i, index) is false)
+        {
+          items.Enqueue(item);
+          _indices[items.Count - 1] = item;
+        }
+
+        i++;
+      }
+
+      _items = items;
+    });
+  }
+
+  /// <summary>
+  /// Enumerate
+  /// </summary>
+  /// <returns></returns>
+  protected IEnumerator<T> Enumerate()
+  {
+    using (var enumerator = _items.GetEnumerator())
+    {
+      var i = 0;
+      var items = new ConcurrentQueue<T>();
+
+      while (enumerator.MoveNext())
+      {
+        var item = _indices.TryGetValue(i++, out T value) ? value : enumerator.Current;
+
+        items.Enqueue(item);
+
+        yield return item;
+      }
+
+      _items = items;
+    }
+  }
+
+  /// <summary>
+  /// Update with index check
+  /// </summary>
+  /// <param name="index"></param>
+  /// <param name="min"></param>
+  /// <param name="max"></param>
+  /// <param name="action"></param>
+  /// <exception cref="ArgumentOutOfRangeException"></exception>
+  protected void UpdateInRange(int index, int min, int max, Action action)
+  {
+    if (index < min || index >= max)
+    {
+      throw new ArgumentOutOfRangeException("Incorrect index");
+    }
+
+    action();
+  }
+
+  /// <summary>
+  /// Enumerate
+  /// </summary>
+  /// <returns></returns>
+  IEnumerator<T> IEnumerable<T>.GetEnumerator() => Enumerate();
+
+  /// <summary>
+  /// Enumerate
+  /// </summary>
+  /// <returns></returns>
+  IEnumerator IEnumerable.GetEnumerator() => Enumerate();
+}
